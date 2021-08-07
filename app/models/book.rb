6b777a508450f9d4329143a7949669ec707e1cce
ammarkandel <merom8703@gmail.com>
2021-08-07 23:15:21 +0200

    Fix linter warnings

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
   validates :status, :title, :author, :description, :progress, :status, :privacy, presence: true
-  STATUS = %i[:done :reading]
-  STATUS.freeze
-  PRIVACY = %i[:share :private]
-  PRIVACY.freeze
+  STATUS = %i[done reading].freeze
+  PRIVACY = %i[share private].freeze
 end
