fb936ea1fe92049ef1b866929307f9066d571d74
ammarkandel <merom8703@gmail.com>
2021-08-09 05:16:21 +0200

    Add navbar and stylesheet base

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -14,4 +14,13 @@ module BooksHelper
     end
     out.html_safe
   end
+
+  def nav_links_access
+    out = ''
+    return unless current_user
+    out << '<li>profile</li>'
+    out << '<li>new book</li>'
+    out << '<li>logout</li>'
+    out.html_safe
+  end
 end
