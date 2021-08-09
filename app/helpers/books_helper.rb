87a29745c951f6190de03e08619e4c2efef780ea
ammarkandel <merom8703@gmail.com>
2021-08-09 06:37:54 +0200

    Add style for navbar

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -14,13 +14,4 @@ module BooksHelper
     end
     out.html_safe
   end
-
-  def nav_links_access
-    out = ''
-    return unless current_user
-    out << '<li>profile</li>'
-    out << '<li>new book</li>'
-    out << '<li>logout</li>'
-    out.html_safe
-  end
 end
