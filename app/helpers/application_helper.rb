e5ce3d797502d89e658a38c556b5610a6a753ef4
ammarkandel <merom8703@gmail.com>
2021-08-12 06:35:40 +0200

    Fix linter warnings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def nav_links_access
     out = ''
     return unless current_user
+
     out << "<li>#{link_to('Profile', user_profile_path)}</li>"
     out << "<li>#{link_to('New Book', new_book_path)}</li>"
     out << "<li>#{link_to 'Logout', destroy_user_session_path, method: :delete}</li>"
@@ -10,7 +11,8 @@ module ApplicationHelper
 
   def nav_links_auth
     out = ''
-    return unless !current_user
+    return if !current_user
+
     out << "<li>#{link_to('Login', new_user_session_path)}</li>"
     out << "<li>#{link_to('Signup', new_user_registration_path)}</li>"
     out.html_safe
