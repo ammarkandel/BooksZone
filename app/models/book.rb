4286df601cdeb2e2d61e994767f6d1fa4ff0936f
ammarkandel <merom8703@gmail.com>
2021-08-12 03:09:49 +0200

    Add test for user model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
+
   validates :title, :author, presence: true, length: { minimum: 3, maximum: 25 }
   validates :progress, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 400 }
+
   STATUS = %i[done reading].freeze
   PRIVACY = %i[share private].freeze
 end
