18826c805a118e3bf0d4a2fc44eacedd51808c75
ammarkandel <merom8703@gmail.com>
2021-10-30 04:53:23 +0200

    handle text in view test

--- spec/feature/views/books_spec.rb
+++ spec/feature/views/books_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 RSpec.describe 'Books Views', type: :feature do
   before(:each) do
-    u1 = User.new({ email: 'test1@s.com', name: 'Test1', password: '123456', password_confirmation: '123456' })
-    u1.save
     visit '/users/sign_in'
     fill_in 'Email', with: 'test1@s.com'
     fill_in 'Password', with: '123456'
